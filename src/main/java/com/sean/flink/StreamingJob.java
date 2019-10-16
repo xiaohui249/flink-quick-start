379e6c1621cc3543e966aa23b93801a7d134c770
sean_on_deepin <xiaohui249@gmail.com>
2019-10-16 13:59:20 +0800

    修改Tuple2类型

--- src/main/java/com/sean/flink/StreamingJob.java
+++ src/main/java/com/sean/flink/StreamingJob.java
@@ -19,11 +19,12 @@
 package com.sean.flink;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
-import scala.Tuple2;
+
 
 /**
  * Skeleton for a Flink Streaming Job.
